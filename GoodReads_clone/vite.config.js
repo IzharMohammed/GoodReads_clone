9429fb84916a9cbddfd04f4fde1065918bd72c15
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-06-03 01:20:04 +0530

    Added absolute import configurations using vite.config and jsconfig.json

--- GoodReads_clone/vite.config.js
+++ GoodReads_clone/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve : {
+    alias :{
+      Components : "/src/Components",
+      Pages : "/src/Pages",
+      Assets : "/src/Assets",
+      Configs : "/src/Configs",
+      Helpers : "/src/Helpers",
+      Layout : "/src/Layouts",
+      Redux : "/src/Redux",
+      Routes : "/src/Routes"
+    }
+  }
 })
